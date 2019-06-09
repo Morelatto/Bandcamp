77f1ab06ca100dc02b4257571fe634bd5b1ca3f1
Morelatto <morelatto@pm.me>
2019-06-08 22:23:19 -0300

    Added parallel album parsing.

--- dl.py
+++ dl.py
@@ -1,8 +1,9 @@
 import json
+import multiprocessing
 import os
 import pymongo
 
-from bandcamp_dl.bandcamp import Bandcamp
+from bandcamp_dl import bandcamp
 from bandcamp_dl.bandcampdownloader import BandcampDownloader
 
 DL_DIR = 'dls/'
@@ -10,13 +11,22 @@ os.makedirs(DL_DIR, exist_ok=True)
 
 DEFAULT_TEMPLATE = '%{artist}/%{album}/%{track} - %{title}'
 
-bandcamp = Bandcamp()
-
 c = pymongo.MongoClient()
 db = c['bandcamp']
 
+MAX_PROCESSES = 4
+
+
+# TODO retry failed
+def parse_album(url):
+    try:
+        return bandcamp.parse(url)
+    except Exception as e:
+        print('Failed to parse album', url, e)
+
 
 def bc_dl(urls, base_dir=DL_DIR):
+    pool = multiprocessing.Pool(MAX_PROCESSES)
     downloader = BandcampDownloader(DEFAULT_TEMPLATE,
                                     base_dir,
                                     overwrite=False,
@@ -26,30 +36,30 @@ def bc_dl(urls, base_dir=DL_DIR):
                                     no_slugify=False,
                                     debugging=False,
                                     urls=urls)
-    for url in urls:
-        try:
-            album = bandcamp.parse(url, lyrics=True)
-            print('\nDownloading', url)
-            downloader.start(album)
-        except Exception as e:
-            print('Failed to download', url, e)
+    albums = pool.imap_unordered(parse_album, urls)
+    for album in albums:
+        if album:
+            print('Downloading album', album['title'])
+            downloader.download_album(album)
 
 
 def daily():
     posts = db['daily'].find({'to_dl': {'$exists': True}}).sort('published', -1)
     for p in posts:
-        # TODO treat /music (AttributeError)
+        # TODO treat /music urls (AttributeError)
         bc_dl(p['to_dl'])
         db['daily'].update_one(p, {'$rename': {'to_dl': 'dl'}})
 
 
 def tags(json_results):
+    print('Starting downloads')
     with open(json_results) as f:
         for collection in parse_scrapy_json(f.readlines()):
             urls = set()
             for release in collection['releases']:
                 urls.add(release['url'])
             bc_dl(urls, os.path.join(DL_DIR, release['genre'], collection['name']))
+    print('Finished downloading. Total', len(urls))
 
 
 # Scrapy creates invalid json
